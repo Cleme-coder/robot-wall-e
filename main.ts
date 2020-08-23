6368470a4a4549a459d3059866605dc9421f948c
Cleme-coder <67659694+Cleme-coder@users.noreply.github.com>
2020-08-23 13:16:19 -0400

    photo resistor

--- main.ts
+++ main.ts
@@ -1,38 +1,8 @@
-let ledr = 0
-let ledb = 0
-let ledg = 0
-PCA9685.reset(67)
+let val = 0
+let strip = neopixel.create(DigitalPin.P5, 18, NeoPixelMode.RGB)
+strip.clear()
 basic.forever(function () {
-    while (true) {
-        for (let index = 0; index < 10; index++) {
-            PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED5, ledr, 67)
-            basic.pause(500)
-            ledr += 10
-        }
-        for (let index = 0; index < 10; index++) {
-            PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED5, ledr, 67)
-            basic.pause(500)
-            ledr += -10
-        }
-        for (let index = 0; index < 10; index++) {
-            PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED6, ledb, 67)
-            basic.pause(500)
-            ledb += 10
-        }
-        for (let index = 0; index < 10; index++) {
-            PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED6, ledb, 67)
-            basic.pause(500)
-            ledb += -10
-        }
-        for (let index = 0; index < 10; index++) {
-            PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, ledg, 67)
-            basic.pause(500)
-            ledg += 10
-        }
-        for (let index = 0; index < 10; index++) {
-            PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED7, ledg, 67)
-            basic.pause(500)
-            ledg += -10
-        }
-    }
+    val = Math.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, 0, 255)
+    strip.showColor(neopixel.rgb(255 - val, 255 - val, 255 - val))
+    basic.pause(100)
 })
