3a37bc9a6517d3a45e795288b499c91e83a5244f
Cleme-coder <67659694+Cleme-coder@users.noreply.github.com>
2020-08-23 19:21:45 -0400

    line tracker

--- main.ts
+++ main.ts
@@ -1,27 +1,55 @@
-let RR = 0
-let LL = 0
+function back () {
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 100, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 35, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 100, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 35, 67)
+}
+function turn_right () {
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 20, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 55, 67)
+}
+function stop () {
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 0, 67)
+}
+function forward () {
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 40, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 40, 67)
+}
+function turn_left () {
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 55, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 20, 67)
+}
+let s2 = 0
+let s1 = 0
+PCA9685.init(67, 0)
+led.enable(false)
+let strip = neopixel.create(DigitalPin.P5, 18, NeoPixelMode.RGB)
+strip.clear()
+PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED5, 100, 67)
+PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED6, 100, 67)
+PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED7, 100, 67)
 basic.forever(function () {
-    RR = pins.digitalReadPin(DigitalPin.P12)
-    LL = pins.digitalReadPin(DigitalPin.P13)
-    if (LL == 1 && RR == 0) {
-        basic.showLeds(`
-            . . . . #
-            . . . . #
-            . . . . #
-            . . . . #
-            . . . . #
-            `)
-    } else if (LL == 0 && RR == 1) {
-        basic.showLeds(`
-            # . . . .
-            # . . . .
-            # . . . .
-            # . . . .
-            # . . . .
-            `)
-    } else if (LL == 1 && RR == 1) {
-        basic.showIcon(IconNames.SmallHeart)
+    strip.showRainbow(1, 360)
+    s1 = pins.digitalReadPin(DigitalPin.P13)
+    s2 = pins.digitalReadPin(DigitalPin.P13)
+    if (s1 == 1 && s2 == 0) {
+        turn_right()
     } else {
-        basic.showIcon(IconNames.Heart)
+        if (s1 == 0 && s2 == 1) {
+            turn_left()
+        } else if (s1 == 0 && s2 == 0) {
+            stop()
+        } else {
+            forward()
+        }
     }
 })
